cf2a78ef92c6ed8252a7995a83fffa7dfa5413c0
ngdiep2312 <ngdiep2312@gmail.com>
2021-12-19 17:34:14 +0700

    add cart-ui
    error image not load

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import SecureView from "./routers/SecureView";
 import HomePage from "./views/HomePage";
 import LoginPage from "./views/Login/LoginPage";
+import Cart from "./views/Cart/Cart";
 import NotFoundPage from "./views/Error/NotFoundPage";
 function Layout(props) {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/cart" component={Cart} />
       {SecureView()}
       <Route component={NotFoundPage} />
     </Switch>
