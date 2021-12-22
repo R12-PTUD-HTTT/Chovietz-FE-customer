2e28b6a16b9943d1ffc74d85aa954413b318a4c5
Khoa <dangkhoa2401bg@gmail.com>
2021-12-22 23:53:20 +0700

    add UI SignUp for Shop

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import SecureView from './routers/SecureView';
-import HomePage from './views/HomePage';
-import LoginPage from './views/Login/LoginPage';
-import Cart from './views/Cart/Cart';
-import NotFoundPage from './views/Error/NotFoundPage';
-import Header from './component/header/Header';
-import Footer from './component/footer/Footer';
-import NavBar from './component/NavBar/NavBar';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import SecureView from "./routers/SecureView";
+import HomePage from "./views/HomePage";
+import LoginPage from "./views/Login/LoginPage";
+import Cart from "./views/Cart/Cart";
+import NotFoundPage from "./views/Error/NotFoundPage";
+import Header from "./component/header/Header";
+import Footer from "./component/footer/Footer";
+import NavBar from "./component/NavBar/NavBar";
+import SignUpPage from "./component/SignUp/SignUp";
 function Layout(props) {
   return (
     <>
@@ -17,6 +18,7 @@ function Layout(props) {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/cart" component={Cart} />
+        <Route exact path="/signup" component={SignUpPage} />
 
         {SecureView()}
         <Route component={NotFoundPage} />
