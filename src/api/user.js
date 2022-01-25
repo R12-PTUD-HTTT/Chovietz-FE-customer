c872eef2190806f23526db61e688a66a90efc57b
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2022-01-25 20:41:43 +0700

    fix bug get userid

--- src/api/user.js
+++ src/api/user.js
@@ -1,18 +1,20 @@
 import { javaAPI } from './config';
-const user = JSON.parse(localStorage.getItem('user'));
 export const fetchLogin = async (data) => {
   return await javaAPI.post('/public/signin', data);
 };
 export const fetchUserProfile = async () => {
+  const user = JSON.parse(localStorage.getItem('user'));
   return await javaAPI.get(`/user/customer/${user.userId}`);
 };
 export const getUserInfor = async (id) => {
   return await javaAPI.get(`/user/customer/${id}`);
 };
 export const updateUserProfile = async (data) => {
+  const user = JSON.parse(localStorage.getItem('user'));
   return await javaAPI.put(`/user/customer/${user.userId}`, data);
 };
 export const changePassword = async (data) => {
+  const user = JSON.parse(localStorage.getItem('user'));
   return await javaAPI.put(`/user/customer/changepass/${user.userId}`, data);
 };
 export const SignUp = async (data) => {
